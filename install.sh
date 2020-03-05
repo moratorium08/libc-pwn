efcef556e71d82953ae073158ed831b0db78b1c3
moratorium08 <moratorium08@gmail.com>
2020-03-05 13:44:48 +0900

    Add install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 set -eu
 
+dpkg --add-architecture i386
 apt-get update && apt-get upgrade
 apt-get -y install \
     python2.7 \
@@ -9,7 +10,10 @@ apt-get -y install \
     libssl-dev \
     libffi-dev \
     socat \
-    procps
+    procps \
+    lib32z1 \
+    libc6-dbg \
+    libc6-dbg:i386
 pip install --upgrade pwntools
 git clone https://github.com/longld/peda.git ~/peda
 cd ~/
